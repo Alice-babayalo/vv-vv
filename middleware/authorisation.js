67e9d0c1d000c911f2a61b7922b1549d1108d156
Alice-babayalo <umuhozawasealice@gmail.com>
2024-06-06 15:46:27 +0300

    Update: authorisation

--- middleware/authorisation.js
+++ middleware/authorisation.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import config from "../configs/index.js";
-import userModel from "../models/user.model.js";
+import userModel from "../model/user.model.js";
 
 const authMiddleware = async (req, res, next) => {
   try {
