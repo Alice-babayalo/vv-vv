cd72f875b04c36316e0aeca290343b3aeb29282d
Alice-babayalo <umuhozawasealice@gmail.com>
2024-06-10 16:32:55 +0200

    using uuid in user model

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from 'uuid';
 
 
 const userchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4
+},
   firstName: {
     type: String,
     required: true,
