572ce66dfbe64cf379859ab383650cb41e7ca26c
Alice-babayalo <umuhozawasealice@gmail.com>
2024-06-10 16:24:28 +0200

    remove the isAdmin middleware in the routes

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { addAlbum, allAlbums, deleteAlbum, updateAlbum } from '../controllers/album.controllers.js'
 import { albumValidation } from '../middleware/validation.js';
 import authMiddleware from '../middleware/authorisation.js';
-import isAdmin from '../middleware/isAdmin.middleware.js';
 
 const albumRoute = express.Router();
 
--- routes/testimony.routes.js
+++ routes/testimony.routes.js
@@ -2,14 +2,13 @@ import express from 'express';
 import { addTestimony, allTestimonies, deleteTestimony, updateTestimony } from '../controllers/testimony.controller.js';
 import { testimonialValidation } from '../middleware/validation.js';
 import authMiddleware from '../middleware/authorisation.js';
-import isAdmin from '../middleware/isAdmin.middleware.js';
 
 const testimonyRoute = express.Router();
 
 testimonyRoute.use(authMiddleware)
 testimonyRoute.post('/add', testimonialValidation, addTestimony)
 testimonyRoute.get('/All', allTestimonies)
-testimonyRoute.delete('/delete/:id', authMiddleware, isAdmin, deleteTestimony)
-testimonyRoute.patch('/update/:id', authMiddleware, isAdmin, testimonialValidation, updateTestimony)
+testimonyRoute.delete('/delete/:id', authMiddleware, deleteTestimony)
+testimonyRoute.patch('/update/:id', authMiddleware, testimonialValidation, updateTestimony)
 
 export default testimonyRoute
