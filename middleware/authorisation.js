95c54a06d0821267968c59b2327d821eedf9b107
Alice-babayalo <umuhozawasealice@gmail.com>
2024-06-10 20:50:30 +0200

    no

--- middleware/authorisation.js
+++ middleware/authorisation.js
@@ -7,7 +7,7 @@ const authMiddleware = async (req, res, next) => {
     // Get the token from the request headers
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized because of invalid token" });
     }
 
     const decodedToken = jwt.verify(token, config.JWT_SECRET_KEY);
@@ -18,7 +18,7 @@ const authMiddleware = async (req, res, next) => {
     next();
   } catch (err) {
     console.error(err);
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized"});
   }
 };
 
